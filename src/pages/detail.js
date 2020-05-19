b5db341e8454bc59e46b2eee34edbc2c9b0307e9
KeonYoungLeee <ygy5036@gmail.com>
2020-05-19 22:56:25 +0900

    Modify detail.js

--- src/pages/detail.js
+++ src/pages/detail.js
@@ -15,32 +15,6 @@ const Detail = ({ movieId }) => {
     });
   }, [movieId]);
 
-
-  // backdrop_path: "/ocUrMYbdjknu2TwzMHKT9PBBQRw.jpg"
-  // belongs_to_collection: null
-  // budget: 42000000
-  // genres: (2) [{…}, {…}]
-  // homepage: "https://www.bloodshot.movie/"
-  // id: 338762
-  // imdb_id: "tt1634106"
-  // original_language: "en"
-  // original_title: "Bloodshot"
-  // overview: "After he and his wife are murdered, marine Ray Garrison is resurrected by a team of scientists. Enhanced with nanotechnology, he becomes a superhuman, biotech killing machine—'Bloodshot'. As Ray first trains with fellow super-soldiers, he cannot recall anything from his former life. But when his memories flood back and he remembers the man that killed both him and his wife, he breaks out of the facility to get revenge, only to discover that there's more to the conspiracy than he thought."
-  // popularity: 182.468
-  // poster_path: "/8WUVHemHFH2ZIP6NWkwlHWsyrEL.jpg"
-  // production_companies: (9) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}]
-  // production_countries: (2) [{…}, {…}]
-  // release_date: "2020-03-05"
-  // revenue: 24573617
-  // runtime: 110
-  // spoken_languages: [{…}]
-  // status: "Released"
-  // tagline: "Being a superhero is in his blood"
-  // title: "Bloodshot"
-  // video: false
-  // vote_average: 7.1
-  // vote_count: 2093
-
   const BackgroundImageSection = styled.div`
     position: relative;
     z-index: -1;
@@ -53,19 +27,30 @@ const Detail = ({ movieId }) => {
     display: flex;
     width: 90vw;
     margin: 0px auto;
-    
+
     & img {
       width: 300px;
       margin-top: -5rem;
       margin-right: 2rem;
     }
 
+    & h2 {
+      font-size: 2.6em;
+      margin: 0.67em 0px;
+    }
+
+    & h2 > span {
+      font-size: 0.7em;
+    }
+
+    & h3 {
+      font-size: 1.4em;
+    }
+
     @media (min-width: 1200px) {
       width: 1200px;
     }
   `
-
-  console.log();
 
   return (
     <div>
@@ -77,7 +62,7 @@ const Detail = ({ movieId }) => {
           <img src={`https://image.tmdb.org/t/p/original/${detailResults.poster_path}`} />
           <div>
             <h2>{detailResults.title} <span>{`(${detailResults.release_date.slice(0,4)})`}</span></h2>
-            <h3>{detailResults.genres.map(((v) => { return <>{v.name}&nbsp;</> }))}<span>{detailResults.runtime}分</span></h3>
+            <h3>{detailResults.genres.map(((v) => { return <>{v.name} | &nbsp;</> }))}<span>{detailResults.runtime}分</span></h3>
             <p>{detailResults.overview}</p>
           </div>
         </PosterSection>
