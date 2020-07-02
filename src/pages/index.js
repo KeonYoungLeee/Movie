058235b5fd8404fed6c4b112fce9bc6cbfae44db
LEEKY <ygy5036@gmail.com>
2020-07-02 19:01:26 +0900

    Modify Code Refactor

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,11 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { TRENDING_MOIVES_REQUEST } from '../reducer/movie';
+import React from 'react';
 import IndexPost from '../container/IndexPost';
 
-
 const Home = () => {
-  const { 
-    movieBackdroppath, 
-    movieTitle } = useSelector(state => state.movie);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch({
-      type: TRENDING_MOIVES_REQUEST,
-    });
-  }, []);
 
   return (
     <>
-      <IndexPost 
-        imgSrc={movieBackdroppath}
-        title={movieTitle}
-      />
+      <IndexPost />
     </>
   );
 };
--- src/pages/movie.js
+++ src/pages/movie.js
@@ -1,40 +1,11 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { CURRENT_MOVIES_REQUEST, UPCOMPING_MOIVES_REQUEST, POPULAR_MOIVES_REQUEST } from '../reducer/movie';
+import React from 'react';
 import MoviePost from '../container/MoviePost';
 
-
 const Movie = () => {
-  const {
-    isLoaded,
-    currentResults,
-    upcompingResults,
-    popularResults } = useSelector(state => state.movie);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch({
-      type: CURRENT_MOVIES_REQUEST,
-    });
-    dispatch({
-      type: UPCOMPING_MOIVES_REQUEST,
-    });
-    dispatch({
-      type: POPULAR_MOIVES_REQUEST,
-    });
-  }, []);
-
+  
   return (
     <>
-      {
-        isLoaded 
-        ? <MoviePost 
-            isLoaded={isLoaded}
-            currentResults={currentResults}
-            upcompingResults={upcompingResults}
-            popularResults={popularResults} /> 
-        : <div>isLoading..</div>
-      }
+      <MoviePost /> 
     </>
   );
 };
