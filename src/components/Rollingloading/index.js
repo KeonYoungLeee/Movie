e4e63259246d7d9584c86f6540b439f77e58c95c
LEEKY <ygy5036@gmail.com>
2020-07-03 08:19:52 +0900

    Create to Roll loading Component

---
+++ src/components/Rollingloading/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Wrapper, LdsRing } from './style';
+
+const Rollingloading = () => {
+  return (
+    <Wrapper>
+      <div>
+      <LdsRing>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </LdsRing>
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Rollingloading;
