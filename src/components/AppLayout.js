e329f64aaa29100f100ea7b34d25606a31219052
KeonYoungLeee <ygy5036@gmail.com>
2020-07-03 21:47:03 +0900

    Modify Code Refactor

--- src/components/AppLayout.js
+++ src/components/AppLayout.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import { Col } from 'antd';
 import PropTypes from 'prop-types';
 import Header from './Header';
-import { RowSylte } from './AppLayout.Style';
+
+import { Row } from 'antd';
 
 const AppLayout = ({ children }) => {
   return (
     <>
       <Header />
-        <RowSylte>
-          <Col >
-            {children}
-          </Col>
-        </RowSylte>
+      <Row> 
+        {children}
+      </Row>
     </>
   );
 };
 
-export default AppLayout;
-
 AppLayout.propTypes = {
   children: PropTypes.node.isRequired,
 };
+
+export default AppLayout;
