d3f135f08463b3d81b34c318d3c97a0be80dcf3a
KeonYoungLeee <ygy5036@gmail.com>
2020-07-07 21:16:14 +0900

    Modify Code ImagesSection

--- src/components/DetailContent/ImagesSection/index.js
+++ src/components/DetailContent/ImagesSection/index.js
@@ -3,40 +3,42 @@ import PropTypes from 'prop-types';
 import Swiper from 'react-id-swiper';
 
 import { Wrapper, Content } from './style';
+import { useSelector } from 'react-redux';
 
-const ImagesSection = ({ results, params }) => {
+const ImagesSection = ({ params }) => {
+
+  const { backdrops } = useSelector(state => state.movieData.Images);
+
   return (
     <>
       <Wrapper>
-          <h2>写真</h2>
-          <Swiper {...params}>
-              {
-                results && results.backdrops.length > 0 && results.backdrops.map((v) => {
-                  return (
-                    <div>
-                      <Content>
-                      {v.file_path === null
-                        ? 
-                          <>
-                            <img src="https://www.freeiconspng.com/uploads/no-image-icon-0.png" width="550" alt="Icon Photos No" />
-                          </>
-                        : <>
-                            <img src={`https://image.tmdb.org/t/p/w300/${v.file_path}`} />
-                          </>
-                      }
-                      </Content>
-                    </div>
-                  );
-                })
-              }
-            </Swiper>
-        </Wrapper>
+        <h2>写真</h2>
+        <Swiper {...params}>
+          {
+            backdrops.map((items) => {
+              return (
+                <div>
+                  <Content>
+                    { items.file_path === null
+                      ? <>
+                          <img src="https://www.freeiconspng.com/uploads/no-image-icon-0.png" width="550" alt="Icon Photos No" />
+                        </>
+                      : <>
+                          <img src={`https://image.tmdb.org/t/p/w300/${items.file_path}`} />
+                        </>
+                    }
+                  </Content>
+                </div>
+              );
+            })
+          }
+        </Swiper>
+      </Wrapper>
     </>
   );
 };
 
 ImagesSection.propTypes = {
-  results: PropTypes.object.isRequired,
   params: PropTypes.object.isRequired,
 };
 
