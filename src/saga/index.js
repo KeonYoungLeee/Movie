543b6c5dd245df481dd18a86b1cf374abf71b80e
KeonYoungLeee <ygy5036@gmail.com>
2020-09-12 17:33:36 +0900

    .env Settings for security

--- src/saga/index.js
+++ src/saga/index.js
@@ -2,7 +2,7 @@ import { all, call } from 'redux-saga/effects';
 import axios from 'axios';
 import movieAPI from './Movie/index';
 
-export const API_KEY = '06221289d9b66d02c4572c58894a7e97';
+export const API_KEY = process.env.API_KEY;
 export const LANGUAGE = 'language=ja-jp';
 
 axios.defaults.baseURL = 'https://api.themoviedb.org/3';
